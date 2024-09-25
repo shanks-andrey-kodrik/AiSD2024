a289c5b16242325a22bb44df83644cbcc5d2930a
Даниил Шарапов <>
2024-09-25 20:13:39 +0500

    Corrected solution v2

--- L303_088.cpp
+++ L303_088.cpp
@@ -21,27 +21,23 @@ void print_list(List* l) {
 }
 
 int main() {
-    List* listt = NULL;
-    /*
     //T1 - ожидаем список 3 2 1
-    insert_list(&listt, 1);
-    insert_list(&listt, 2);
-    insert_list(&listt, 3);
-    print_list(listt);
-    */
+    List* listt1 = NULL;
+    insert_list(&listt1, 1);
+    insert_list(&listt1, 2);
+    insert_list(&listt1, 3);
+    print_list(listt1);
     
-    /*
     //T2 - ожидаем пустую строку
-    print_list(listt);
-    */
+    List* listt2 = NULL;
+    print_list(listt2);
     
-    /*
     //T3 - проверка порядка вставки
+    List* listt3 = NULL;
     for (int i = 0; i < 5; i++){
-        insert_list(&listt, i);
+        insert_list(&listt3, i);
     }
-    print_list(listt);
-    */
+    print_list(listt3);
     return 0;
     
 }
