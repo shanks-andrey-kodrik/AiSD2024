4f5ff6a5a7bf829b46828c8ffc336ebdd3d179e2
Даниил Шарапов <>
2024-10-01 00:22:48 +0500

    Sharapov Daniil, PM-201

---
+++ L303_088/L303_088.c
@@ -0,0 +1,25 @@
+//L303_088
+//Вставка элемента в однонаправленный связный список
+//ПМ-2024 Шарапов Даниил, 25.09.24
+#include <iostream>
+#include "L303_088.h"
+using namespace std;
+struct List {
+    int value;
+    List* next;
+};
+
+void insert_list(List** l, int x) {
+    List* p = new List;
+    p->value = x;
+    p->next = *l;      
+    *l = p;            
+}
+
+void print_list(List* l) {
+    while (l != NULL) {
+        cout << l->value << " ";
+        l = l->next;
+    }
+    cout << endl;
+}
