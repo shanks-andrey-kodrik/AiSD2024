ec13b546033ab3fa2f09dd1330719f591120706e
Даниил Шарапов <>
2024-10-01 19:09:00 +0500

    Sharapov Daniil, PM-201

---
+++ L302_087/L302_087.c
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "L302_087.h"
+using namespace std;
+struct List{
+    int value;
+    List* next;
+};
+
+List* search_list(List* l, int x){//основная функция
+    if (l == NULL) return NULL;
+    if (l->value == x) return l;
+    return search_list(l->next, x);
+}
+
+List* append(List* head, int value){//вспомогательная функция для тестов, которая добавляет элементы в список
+    List* newList = new List{value, NULL};
+    if (head == NULL) return newList;
+    List* current = head;
+    while (current->next != NULL){
+        current = current->next;
+    }
+    current->next = newList;
+    return head;
+}
---
+++ L302_087/L302_087.h
@@ -0,0 +1,6 @@
+#ifndef KVA_H
+#define KVA_H
+struct List;
+List* search_list(List* l, int x);
+List* append(List* head, int value);
+#endif
